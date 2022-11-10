1858cd8485e591a977bc4c52ce4bfd0685d37312
kukeee13 <*ryan.danta951@gmail.com*>
2022-11-10 07:47:57 +0700

    master

---
+++ index.js
@@ -0,0 +1,471 @@
+/* Created By danta Bot WhatsApp */
+/* WhatsApp Creator Di Bawah */
+/* wa.me/6287705048235 */
+
+const { modul } = require('./module');
+const { baileys, boom, chalk, fs, figlet, FileType, path, pino, process, PhoneNumber } = modul;
+const { Boom } = boom
+const { default: dantaConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = baileys
+const { color, bgcolor } = require('./lib/color')
+const colors = require('colors')
+const { uncache, nocache } = require('./lib/loader')
+const { state } = useSingleFileAuthState(`./kayla.json`)
+const { start } = require('./lib/spinner')
+const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif')
+const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep, reSize } = require('./lib/myfunc')
+
+const owner = JSON.parse(fs.readFileSync('./database/owner.json'))
+
+const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+
+require('./danta.js')
+nocache('../danta.js', module => console.log(color('[ UPDATE ]', 'cyan'), color(`'${module}'`, 'green'), 'File Is Update!!!'))
+require('./index.js')
+nocache('../index.js', module => console.log(color('[ UPDATE ]', 'cyan'), color(`'${module}'`, 'green'), 'File Is Update!!!'))
+
+async function dantaBot() {
+const { version, isLatest } = await fetchLatestBaileysVersion()
+const danta = dantaConnect({
+logger: pino({ level: 'silent' }),
+printQRInTerminal: true,
+browser: ['DANTA DEV','Safari','1.0.0'],
+auth: state,
+version
+})
+
+store.bind(danta.ev)
+
+console.log(color(figlet.textSync(`danta`, {
+font: 'Standard',
+horizontalLayout: 'default',
+vertivalLayout: 'default',
+whitespaceBreak: false
+}), 'green'))
+
+danta.ws.on('CB:Blocklist', json => {
+if (blocked.length > 2) return
+for (let i of json[1].blocklist) {
+blocked.push(i.replace('c.us','s.whatsapp.net'))}})
+
+danta.ws.on('CB:call', async (json) => {
+const callerId = json.content[0].attrs['call-creator']
+const idCall = json.content[0].attrs['call-id']
+const Id = json.attrs.id
+const T = json.attrs.t
+danta.sendNode({
+  tag: 'call',
+    attrs: {
+      from: '6281356484612@s.whatsapp.net',
+      id: Id,
+      t: T
+    },
+    content: [
+      {
+        tag: 'reject',
+        attrs: {
+          'call-creator': callerId,
+          'call-id': idCall,
+          count: '0'
+        },
+        content: null
+      }
+    ]
+})
+if (json.content[0].tag == 'offer') {
+let qutsnya = await danta.sendContact(callerId, owner)
+await danta.sendMessage(callerId, { text: `Sistem Otomatis Block!!!\nJangan Menelpon Bot!!!\nSilahkan Hubungi Owner Untuk Dibuka!!!`}, { quoted : qutsnya })
+await sleep(8000)
+await danta.updateBlockStatus(callerId, "block")
+}
+})
+
+danta.ev.on('messages.upsert', async chatUpdate => {
+try {
+kay = chatUpdate.messages[0]
+if (!kay.message) return
+kay.message = (Object.keys(kay.message)[0] === 'ephemeralMessage') ? kay.message.ephemeralMessage.message : kay.message
+if (kay.key && kay.key.remoteJid === 'status@broadcast') return
+if (!danta.public && !kay.key.fromMe && chatUpdate.type === 'notify') return
+if (kay.key.id.startsWith('BAE5') && kay.key.id.length === 16) return
+m = smsg(danta, kay, store)
+require('./danta')(danta, m, chatUpdate, store)
+} catch (err) {
+console.log(err)}})
+
+danta.ev.on('group-participants.update', async (anu) => {
+console.log(anu)
+try {
+let metadata = await danta.groupMetadata(anu.id)
+let participants = anu.participants
+for (let num of participants) {
+try {
+ppuser = await danta.profilePictureUrl(num, 'image')
+} catch {
+ppuser = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+}
+try {
+ppgroup = await danta.profilePictureUrl(anu.id, 'image')
+} catch {
+ppgroup = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+}
+if (anu.action == 'add') {
+danta.sendMessage(anu.id, { text : `*Halo @${num.split("@")[0]}* 😱🗿`, mentions : [num]},{ quoted : {
+key: {
+fromMe: false, 
+participant: `0@s.whatsapp.net`, 
+...({ remoteJid: "status@broadcast" }) 
+},
+"message": {
+"pollCreationMessage": {
+"name": `Welcome Banh👋😁`,
+"options": [
+	{
+"optionName": "KATANYA WA KEBAL"
+	},
+	{
+"optionName": "BERANI VOTE GA"
+	},
+	{
+"optionName": "VOTE LAH SEMUA"
+	},
+	{
+"optionName": "KATANYA KEBAL"
+	},
+	{
+"optionName": "SALAM DARI DANTA DEV"
+	}
+],
+"selectableOptionsCount": 5
+}}}})
+} else if (anu.action == 'remove') {
+danta.sendMessage(anu.id, { text : `*Selamat tinggal @${num.split("@")[0]}* 👋🗿`, mentions : [num]},{ quoted : {
+key: {
+fromMe: false, 
+participant: `0@s.whatsapp.net`, 
+...({ remoteJid: "status@broadcast" }) 
+},
+"message": {
+"pollCreationMessage": {
+"name": `Sayonara👋`,
+"options": [
+	{
+"optionName": "KATANYA WA KEBAL"
+	},
+	{
+"optionName": "BERANI VOTE GA"
+	},
+	{
+"optionName": "VOTE LAH SEMUA"
+	},
+	{
+"optionName": "KATANYA KEBAL"
+	},
+	{
+"optionName": "SALAM DARI DANTA DEV"
+	}
+],
+"selectableOptionsCount": 5
+}}}})
+}
+}
+} catch (err) {
+console.log(err)
+}
+})
+
+danta.decodeJid = (jid) => {
+if (!jid) return jid
+if (/:\d+@/gi.test(jid)) {
+let decode = jidDecode(jid) || {}
+return decode.user && decode.server && decode.user + '@' + decode.server || jid
+} else return jid
+}
+
+danta.ev.on('contacts.update', update => {
+for (let contact of update) {
+let id = danta.decodeJid(contact.id)
+if (store && store.contacts) store.contacts[id] = { id, name: contact.notify }
+}
+})
+
+danta.getName = (jid, withoutContact  = false) => {
+id = danta.decodeJid(jid)
+withoutContact = danta.withoutContact || withoutContact 
+let v
+if (id.endsWith("@g.us")) return new Promise(async (resolve) => {
+v = store.contacts[id] || {}
+if (!(v.name || v.subject)) v = danta.groupMetadata(id) || {}
+resolve(v.name || v.subject || PhoneNumber('+' + id.replace('@s.whatsapp.net', '')).getNumber('international'))
+})
+else v = id === '0@s.whatsapp.net' ? {
+id,
+name: 'WhatsApp'
+} : id === danta.decodeJid(danta.user.id) ?
+danta.user :
+(store.contacts[id] || {})
+return (withoutContact ? '' : v.name) || v.subject || v.verifiedName || PhoneNumber('+' + jid.replace('@s.whatsapp.net', '')).getNumber('international')
+}
+
+danta.parseMention = (text = '') => {
+return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')
+}
+
+danta.sendContact = async (jid, kon, quoted = '', opts = {}) => {
+let list = []
+for (let i of kon) {
+list.push({
+displayName: await danta.getName(i + '@s.whatsapp.net'),
+vcard: `BEGIN:VCARD\n
+VERSION:3.0\n
+N:${await danta.getName(i + '@s.whatsapp.net')}\n
+FN:${await danta.getName(i + '@s.whatsapp.net')}\n
+item1.TEL;waid=${i}:${i}\n
+item1.X-ABLabel:Ponsel\n
+item2.EMAIL;type=INTERNET:Faruqofc/.my.id\n
+item2.X-ABLabel:Email\n
+item3.URL:https://bit.ly/39Ivus6\n
+item3.X-ABLabel:YouTube\n
+item4.ADR:;;Indonesia;;;;\n
+item4.X-ABLabel:Region\n
+END:VCARD`
+})
+}
+danta.sendMessage(jid, { contacts: { displayName: `${list.length} Kontak`, contacts: list }, ...opts }, { quoted })
+}
+
+danta.setStatus = (status) => {
+danta.query({
+tag: 'iq',
+attrs: {
+to: '@s.whatsapp.net',
+type: 'set',
+xmlns: 'status',
+},
+content: [{
+tag: 'status',
+attrs: {},
+content: Buffer.from(status, 'utf-8')
+}]
+})
+return status
+}
+
+danta.public = true
+
+danta.sendImage = async (jid, path, caption = '', quoted = '', options) => {
+let buffer = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+return await danta.sendMessage(jid, { image: buffer, caption: caption, ...options }, { quoted })
+}
+
+danta.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
+let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+let buffer
+if (options && (options.packname || options.author)) {
+buffer = await writeExifImg(buff, options)
+} else {
+buffer = await imageToWebp(buff)
+}
+await danta.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+return buffer
+}
+
+danta.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
+let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+let buffer
+if (options && (options.packname || options.author)) {
+buffer = await writeExifVid(buff, options)
+} else {
+buffer = await videoToWebp(buff)
+}
+await danta.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+return buffer
+}
+
+danta.copyNForward = async (jid, message, forceForward = false, options = {}) => {
+let vtype
+if (options.readViewOnce) {
+message.message = message.message && message.message.ephemeralMessage && message.message.ephemeralMessage.message ? message.message.ephemeralMessage.message : (message.message || undefined)
+vtype = Object.keys(message.message.viewOnceMessage.message)[0]
+delete(message.message && message.message.ignore ? message.message.ignore : (message.message || undefined))
+delete message.message.viewOnceMessage.message[vtype].viewOnce
+message.message = {
+...message.message.viewOnceMessage.message
+}
+}
+let mtype = Object.keys(message.message)[0]
+let content = await generateForwardMessageContent(message, forceForward)
+let ctype = Object.keys(content)[0]
+let context = {}
+if (mtype != "conversation") context = message.message[mtype].contextInfo
+content[ctype].contextInfo = {
+...context,
+...content[ctype].contextInfo
+}
+const waMessage = await generateWAMessageFromContent(jid, content, options ? {
+...content[ctype],
+...options,
+...(options.contextInfo ? {
+contextInfo: {
+...content[ctype].contextInfo,
+...options.contextInfo
+}
+} : {})
+} : {})
+await danta.relayMessage(jid, waMessage.message, { messageId:  waMessage.key.id })
+return waMessage
+}
+
+danta.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
+let quoted = message.msg ? message.msg : message
+let mime = (message.msg || message).mimetype || ''
+let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+const stream = await downloadContentFromMessage(quoted, messageType)
+let buffer = Buffer.from([])
+for await(const chunk of stream) {
+buffer = Buffer.concat([buffer, chunk])
+}
+let type = await FileType.fromBuffer(buffer)
+trueFileName = attachExtension ? (filename + '.' + type.ext) : filename
+await fs.writeFileSync(trueFileName, buffer)
+return trueFileName
+}
+
+danta.downloadMediaMessage = async (message) => {
+let mime = (message.msg || message).mimetype || ''
+let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+const stream = await downloadContentFromMessage(message, messageType)
+let buffer = Buffer.from([])
+for await(const chunk of stream) {
+buffer = Buffer.concat([buffer, chunk])
+}
+return buffer
+}
+
+danta.getFile = async (PATH, save) => {
+let res
+let data = Buffer.isBuffer(PATH) ? PATH : /^data:.*?\/.*?;base64,/i.test(PATH) ? Buffer.from(PATH.split`,`[1], 'base64') : /^https?:\/\//.test(PATH) ? await (res = await getBuffer(PATH)) : fs.existsSync(PATH) ? (filename = PATH, fs.readFileSync(PATH)) : typeof PATH === 'string' ? PATH : Buffer.alloc(0)
+let type = await FileType.fromBuffer(data) || {
+mime: 'application/octet-stream',
+ext: '.bin'}
+filename = path.join(__filename, './lib' + new Date * 1 + '.' + type.ext)
+if (data && save) fs.promises.writeFile(filename, data)
+return {
+res,
+filename,
+size: await getSizeMedia(data),
+...type,
+data}}
+
+danta.sendMedia = async (jid, path, fileName = '', caption = '', quoted = '', options = {}) => {
+let types = await danta.getFile(path, true)
+let { mime, ext, res, data, filename } = types
+if (res && res.status !== 200 || file.length <= 65536) {
+try { throw { json: JSON.parse(file.toString()) } }
+catch (e) { if (e.json) throw e.json }}
+let type = '', mimetype = mime, pathFile = filename
+if (options.asDocument) type = 'document'
+if (options.asSticker || /webp/.test(mime)) {
+let { writeExif } = require('./lib/exif')
+let media = { mimetype: mime, data }
+pathFile = await writeExif(media, { packname: options.packname ? options.packname : global.packname, author: options.author ? options.author : global.author, categories: options.categories ? options.categories : [] })
+await fs.promises.unlink(filename)
+type = 'sticker'
+mimetype = 'image/webp'}
+else if (/image/.test(mime)) type = 'image'
+else if (/video/.test(mime)) type = 'video'
+else if (/audio/.test(mime)) type = 'audio'
+else type = 'document'
+await danta.sendMessage(jid, { [type]: { url: pathFile }, caption, mimetype, fileName, ...options }, { quoted, ...options })
+return fs.promises.unlink(pathFile)}
+
+danta.sendText = (jid, text, quoted = '', options) => danta.sendMessage(jid, { text: text, ...options }, { quoted })
+
+danta.serializeM = (m) => smsg(danta, m, store)
+
+danta.ev.on('connection.update', async (update) => {
+const { connection, lastDisconnect } = update	
+if (connection === 'close') {
+let reason = new Boom(lastDisconnect?.error)?.output.statusCode
+if (reason === DisconnectReason.badSession) { console.log(`Bad Session File, Please Delete Session and Scan Again`); danta.logout(); }
+else if (reason === DisconnectReason.connectionClosed) { console.log("Connection closed, reconnecting...."); dantaBot(); }
+else if (reason === DisconnectReason.connectionLost) { console.log("Connection Lost from Server, reconnecting..."); dantaBot(); }
+else if (reason === DisconnectReason.connectionReplaced) { console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); danta.logout(); }
+else if (reason === DisconnectReason.loggedOut) { console.log(`Device Logged Out, Please Scan Again And Run.`); danta.logout(); }
+else if (reason === DisconnectReason.restartRequired) { console.log("Restart Required, Restarting..."); dantaBot(); }
+else if (reason === DisconnectReason.timedOut) { console.log("Connection TimedOut, Reconnecting..."); dantaBot(); }
+else danta.end(`Unknown DisconnectReason: ${reason}|${connection}`)
+}
+console.log('Connected...', update)
+})
+
+start('2',colors.bold.white('\nMenunggu Pesan Baru..'))
+
+danta.sendButtonText = (jid, buttons = [], text, footer, quoted = '', options = {}) => {
+let buttonMessage = {
+text,
+footer,
+buttons,
+headerType: 2,
+...options
+}
+danta.sendMessage(jid, buttonMessage, { quoted, ...options })
+}
+
+danta.sendKatalog = async (jid , title = '' , desc = '', gam , options = {}) =>{
+let message = await prepareWAMessageMedia({ image: gam }, { upload: danta.waUploadToServer })
+const tod = generateWAMessageFromContent(jid,
+{"productMessage": {
+"product": {
+"productImage": message.imageMessage,
+"productId": "9999",
+"title": title,
+"description": desc,
+"currencyCode": "IDR",
+"priceAmount1000": "100000",
+"url": `https://youtube.com/@dantadev`,
+"productImageCount": 1,
+"salePriceAmount1000": "0"
+},
+"businessOwnerJid": `6281356484612@s.whatsapp.net`
+}
+}, options)
+return danta.relayMessage(jid, tod.message, {messageId: tod.key.id})
+} 
+
+danta.send5ButLoc = async (jid , text = '' , footer = '', img, but = [], options = {}) =>{
+var template = generateWAMessageFromContent(jid, proto.Message.fromObject({
+templateMessage: {
+hydratedTemplate: {
+"hydratedContentText": text,
+"locationMessage": {
+"jpegThumbnail": img },
+"hydratedFooterText": footer,
+"hydratedButtons": but
+}
+}
+}), options)
+danta.relayMessage(jid, template.message, { messageId: template.key.id })
+}
+
+danta.sendButImg = async (jid, path, teks, fke, but) => {
+let img = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+let fjejfjjjer = {
+image: img, 
+jpegThumbnail: img,
+caption: teks,
+fileLength: "1",
+footer: fke,
+buttons: but,
+headerType: 4,
+}
+danta.sendMessage(jid, fjejfjjjer, { quoted: m })
+}
+
+return danta
+}
+
+dantaBot()
+
+process.on('uncaughtException', function (err) {
+console.log('Caught exception: ', err)
+})
